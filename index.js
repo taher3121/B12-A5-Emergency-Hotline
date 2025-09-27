d77c904c6d9f23a8289e1d68ef490c0c9008154c
taher3121 <abutaher01.at@gmail.com>
2025-09-28 01:38:19 +0600

    added history dom

--- index.js
+++ index.js
@@ -23,9 +23,9 @@ document.getElementById("cart-container").addEventListener('click', function (e)
     const getCoinValue = Number(getElement("coin").innerText);
 
     if (getCoinValue >= 20) {
-        
-        if(e.target.className.includes("call-now")) {
-            
+
+        if (e.target.className.includes("call-now") || e.target.className.includes("fa-phone")) {
+
             const callNow = e.target;
             const serviceName = callNow.parentNode.parentNode.children[1].children[0].innerText
             const serviceNumber = callNow.parentNode.parentNode.children[2].children[0].innerText
@@ -34,11 +34,42 @@ document.getElementById("cart-container").addEventListener('click', function (e)
             const updateCoin = getCoin - 20;
             getElement("coin").innerText = updateCoin;
 
+            const history = getElement("call-history")
+
+            const newHistory = document.createElement("div")
+
+            newHistory.innerHTML = `
+                <div class="flex justify-between p-4 items-center bg-gray-100 rounded-2xl my-1">
+                    <div>
+                        <p class="font-[600] text-[15px]">${serviceName}</p>
+                        <p class="text-[12px] text-gray-700">${serviceNumber}</p>
+                    </div>
+                    <p>${new Date().toLocaleTimeString()}</p>
+                </div>
+            
+            `
+            history.append(newHistory);
+
 
         }
     }
-    else{
+    else {
         alert('Have Insufficient coin to call..........')
     }
 
+})
+
+document.getElementById("clear").addEventListener('click',function(){
+    const history = getElement("call-history");
+    history.innerHTML=""
+})
+
+
+document.getElementById("cart-container").addEventListener('click', function(e){
+    if (e.target.className.includes("copy-btn") || e.target.className.includes("fa-copy")){
+        console.log('hello')
+        const copyCount = Number(getElement("copy-count").innerText);
+        const newCount= copyCount+1
+        getElement("copy-count").innerText= newCount
+    }
 })
